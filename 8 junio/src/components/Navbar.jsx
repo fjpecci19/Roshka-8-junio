78257f2b0e5388b03e643840a16375e895414fe7
Franco Pecci <fapeccisan@gmail.com>
2023-06-08 17:08:00 -0400

    a

--- 8 junio/src/components/Navbar.jsx
+++ 8 junio/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     <div className="container-fluid bg-custom">
         <nav className="navbar navbar-expand">
             <div className="container-fluid">
-                <a className="navbar-brand custom-brand"><img className='perro' src="images/perro.png"/></a>
+                <a className="navbar-brand custom-brand"><img className='perro' src="https://images.ecestaticos.com/gf4efC_q7vXFY_l6TEJqriW88w8=/0x87:2155x1283/600x450/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F8ec%2F08c%2F85c%2F8ec08c85c866ccb70c4f1c36492d890f.jpg"/></a>
             </div>
             <div className="container-fluid">
                 <a className="navbar-brand custom-brand">Home</a>
--- 8 junio/src/components/Textos.jsx
+++ 8 junio/src/components/Textos.jsx
@@ -8,19 +8,37 @@ function Textos() {
             <div className='row'>
                 <div className='col-md-6'>
                     <picture>
-                        <source srcSet='images/perrofeliz.png' />
-                        <img src="images/perrofeliz.png" alt="" />
+                        <source srcSet='https://nubika.es/wp-content/uploads/2020/10/como-cuidar-pelo-husky.jpg.webp' />
+                        <img src="https://nubika.es/wp-content/uploads/2020/10/como-cuidar-pelo-husky.jpg.webp" alt="" />
                     </picture>
                 </div>
                 <div className='col-md-6'>
                     <br />
                     <br />
-                    Busca Mascota es una plataforma open-source, para que mascotas perdidas puedan reencontrarse con sus dueños. <br />
-                    Puedes publicar tu mascota sin costo! <br />
+                    Busca Mascota es una plataforma open-source, para que mascotas perdidas puedan reencontrarse con sus dueños. <br /><br />
+                    Puedes publicar tu mascota sin costo! <br /><br />
                     Y también será publicado en las redes oficiales de Busca Mascota automáticamente. <br /><br />
                     <button type="button" class="btn btn-secondary">Buscar Mascota</button>
                     <span className='sep'></span>
                     <button type="button" class="btn btn-secondary">Publicar Mascota</button>
+                </div>
+            </div>
+        </div>
+        <div className='container-fluid custom2'>
+            <div className='row'>
+                <div className='col-md-6'>
+                    <br />
+                    <br />
+                    ¿Quién puede utilizar el sitio?<br /><br />
+                    - Personas que han perdido su mascota y desean publicar un reporte o buscar su mascota.<br /><br />
+                    - Personas que han encontrado o avistado una mascota que parecía perdida y desean publicar un reporte.<br /><br />
+                    Todo con el fin de que estas puedan reencontrarse con sus dueños.<br />
+                </div>
+                <div className='col-md-6'>
+                    <picture>
+                        <source srcSet='https://www.infobae.com/new-resizer/d76jNjx4IUq7J0J91SurdGirOtg=/992x558/filters:format(webp):quality(85)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/09/11124552/GettyImages-544673512.jpg' />
+                        <img src="https://www.infobae.com/new-resizer/d76jNjx4IUq7J0J91SurdGirOtg=/992x558/filters:format(webp):quality(85)/s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/09/11124552/GettyImages-544673512.jpg" alt="" />
+                    </picture>
                 </div>
             </div>
         </div>
