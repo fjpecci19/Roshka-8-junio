56b42a0380f6dfe2c8e0227ef68a6011d5dbf0b9
Franco Pecci <fapeccisan@gmail.com>
2023-06-09 13:29:42 -0400

    a

--- 8 junio/src/main.jsx
+++ 8 junio/src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Publicar from './views/Publicar.jsx'
 
 const router = createBrowserRouter[
   {
     path: "/",
-    element: "App"
+    element: <App/>
+  },
+  {
+    path: "Publicar",
+    element: <Publicar/>
   }
 ]
 
